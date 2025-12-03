c38afcacc07088fe86c692ecb5de8de2e7cf9345
Thành Phát <91819307+Kidynein@users.noreply.github.com>
2025-12-03 14:17:44 +0700

    fix: Adjust page route for the web (#4)

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,5 +1,4 @@
-// src/App.jsx
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import {
   HomePage,
@@ -15,25 +14,19 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/*Main layout and outlet for all pages*/}
-        <Route path="/" element={<MainLayout />}>
-          {/* default page when entry*/}
-          <Route index element={<HomePage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route element={<MainLayout />}>
+          <Route path="/home" element={<HomePage />} />
 
-          {/* List of path*/}
-          <Route path ="signup" element={<SignUpPage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="forgot-password" element={<ForgotPasswordPage />} />
-          <Route path="dashboard" element={<DashboardPage />} />
-          <Route path="my-progress" element={<ProgressPage />} />
-          <Route path="history" element={<HistoryPage />} />
-
-          {/* 404 */}
-          <Route
-            path="*"
-            element={<div className="p-10">404 - Not Found</div>}
-          />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/my-progress" element={<ProgressPage />} />
+          <Route path="/history" element={<HistoryPage />} />
         </Route>
+        {/* 404 - Not Found */}
+        <Route path="*" element={<div className="p-10">404 - Not Found</div>} />
       </Routes>
     </BrowserRouter>
   );
